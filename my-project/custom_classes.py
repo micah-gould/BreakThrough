943996ac4d67c544f0be1ccc5138bf5ef734609d
micah-gould <Gouldmicah@gmail.com>
2025-08-24 19:15:25 +0200

    24.08.25 19:15:00

--- my-project/custom_classes.py
+++ my-project/custom_classes.py
@@ -7,21 +7,21 @@ def round_down_to_even(n):
     return int(np.floor(n / 2.0)) * 2
 
 class SpaceTimeGrid:
-    def __init__(self, scene, v):
+    def __init__(self, scene, speed=0, max_number=24, count=2):
         # Create the coordinate system
         self.scene = scene
-        self.speed = ValueTracker(v)
+        self.speed = ValueTracker(speed)
 
-        self.max_number = 24
-        self.count = 2
+        self.max_number = max_number
+        self.count = count
         self.scale_factor = 0.3
 
         self.grid = Axes(
             x_range=[-self.max_number, self.max_number, self.count],
             y_range=[-self.max_number, self.max_number, self.count],
-            x_length=self.max_number,  # Enlarged so it looks good after scaling
-            y_length=self.max_number,
-            axis_config={"include_numbers": True, "stroke_width": 32 / self.max_number},
+            x_length=24,  # Enlarged so it looks good after scaling
+            y_length=24,
+            axis_config={"include_numbers": True, "stroke_width": 4 / 3},
         ).scale(self.scale_factor).move_to(ORIGIN)
 
         # Label the axes
@@ -29,8 +29,8 @@ class SpaceTimeGrid:
         ct_label = Text("Time (ct)", font_size=36).scale(self.scale_factor)
 
         # Position the labels
-        x_label.next_to(self.grid @ (self.max_number, self.count), UP).shift(DOWN * (self.scale_factor + 0.1))
-        ct_label.next_to(self.grid @ (self.count, self.max_number), RIGHT).shift(LEFT * (self.scale_factor + 0.1))
+        x_label.next_to(self.grid @ (self.max_number, self.max_number / 12), UP).shift(DOWN * (self.scale_factor + 0.1))
+        ct_label.next_to(self.grid @ (self.max_number / 12, self.max_number), RIGHT).shift(LEFT * (self.scale_factor + 0.1))
         
         def make_number_line(flip=1):
 
@@ -39,14 +39,14 @@ class SpaceTimeGrid:
 
             if abs(speed) <= 0.99:
                 nl = NumberLine(
-                    x_range=[round_up_to_even(-self.max_number * (1 - speed**2)**(1/2)), round_down_to_even(self.max_number * (1 - speed**2)**(1/2)), 2],
+                    x_range=[round_up_to_even(-self.max_number * (1 - speed**2)**(1/2)), round_down_to_even(self.max_number * (1 - speed**2)**(1/2)), self.count],
                     length=np.linalg.norm(self.grid @ (-self.max_number, -speed * self.max_number) - self.grid @ (self.max_number, speed * self.max_number)) / self.scale_factor,
                     include_numbers=True,
                     include_tip=True,
                     exclude_origin_tick=True,
-                    numbers_to_exclude=list(range(-3, 4)),
+                    numbers_to_exclude=list(range(-(self.count), self.count + 1)),
                     color=YELLOW,
-                    stroke_width = 32 / self.max_number
+                    stroke_width = 4 / 3
                 )
 
                 tangent = nl @ (1) - nl @ (0)
@@ -65,18 +65,18 @@ class SpaceTimeGrid:
                     include_tip=True,
                     exclude_origin_tick=True,
                     color=YELLOW,
-                    stroke_width = 32 / self.max_number
+                    stroke_width = 4 / 3
                 )
             nl.rotate(angle, about_point=ORIGIN).scale(self.scale_factor).shift(self.grid @ (0, 0) - nl @ (0))
             return nl
 
         self.x__line = always_redraw(lambda: make_number_line(1))
         
-        x__label = always_redraw(lambda: Text("Space (x')", font_size=36).move_to(self.grid @ (self.max_number, self.speed.get_value() * self.max_number + 1)).scale(self.scale_factor))
+        x__label = always_redraw(lambda: Text("Space (x')", font_size=36).move_to(self.grid @ (self.max_number, (self.speed.get_value() + 1/24) * self.max_number)).scale(self.scale_factor))
 
         self.ct__line = always_redraw(lambda: make_number_line(-1))
         
-        ct__label = always_redraw(lambda: Text("Time (ct')", font_size=36).move_to(self.grid @ (self.speed.get_value() * self.max_number + 3, self.max_number - 0.5)).scale(self.scale_factor))
+        ct__label = always_redraw(lambda: Text("Time (ct')", font_size=36).move_to(self.grid @ ((self.speed.get_value() + 1/8) * self.max_number, self.max_number * 47/48)).scale(self.scale_factor))
 
         # Group all elements
         self.everything = VGroup(self.grid, x_label, ct_label, self.x__line, x__label, self.ct__line, ct__label)
@@ -87,8 +87,8 @@ class SpaceTimeGrid:
         self.scene.add(self.everything)
         return
 
-    def create(self):
-        self.scene.play(Create(*self.everything))
+    def create(self, **kwargs):
+        self.scene.play(Create(self.everything), **kwargs)
         return
 
     def remove(self):
@@ -117,7 +117,7 @@ class Location:
     def _x(self, x, ct): return (x - self.diagram.speed.get_value() * ct) / (1 - self.diagram.speed.get_value()**2)**(1/2)
     def _ct(self, x, ct): return (ct - self.diagram.speed.get_value() * x) / (1 - self.diagram.speed.get_value()**2)**(1/2)
 
-    def plot_lines(self):
+    def plot_lines(self, **kwargs):
         # Create the lines
         x_line = always_redraw(
             lambda: Line(self.diagram.grid @ (self.x.get_value(), 0), self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), stroke_width=1)
@@ -126,40 +126,40 @@ class Location:
             lambda: Line(self.diagram.grid @ (0, self.ct.get_value()), self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), stroke_width=1)
         )
         self.everything.add(x_line, ct_line)
-        self.diagram.scene.play(Create(x_line), Create(ct_line))
+        self.diagram.scene.play(Create(x_line), Create(ct_line), **kwargs)
         self.diagram.scene.wait(0.1)
         return
 
-    def plot_point(self):
+    def plot_point(self, **kwargs):
         # Create the point
         point = always_redraw(
             lambda: Dot(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), color=RED, radius=0.04)
         )
         self.everything.add(point)
-        self.diagram.scene.play(Create(point))
+        self.diagram.scene.play(Create(point), **kwargs)
         self.diagram.scene.wait(0.1)
         return
 
-    def plot__lines(self):
+    def plot__lines(self, **kwargs):
         # Create the lines 
         x__line = always_redraw(
-            lambda: Line(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), self.diagram.grid @ self.diagram.prime_to_cords(self._x(self.x.get_value(), self.ct.get_value()), 0), color=YELLOW, stroke_width=0.5)
+            lambda: Line(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), self.diagram.x__line @ self._x(self.x.get_value(), self.ct.get_value()), color=YELLOW, stroke_width=0.5)
         )
         ct__line = always_redraw(
-            lambda: Line(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), self.diagram.grid @ self.diagram.prime_to_cords(0, self._ct(self.x.get_value(), self.ct.get_value())), color=YELLOW, stroke_width=0.5)
+            lambda: Line(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), self.diagram.ct__line @ self._ct(self.x.get_value(), self.ct.get_value()), color=YELLOW, stroke_width=0.5)
         )
         self.everything.add(x__line, ct__line)
-        self.diagram.scene.play(Create(x__line), Create(ct__line))
+        self.diagram.scene.play(Create(x__line), Create(ct__line), **kwargs)
         self.diagram.scene.wait(0.1)
         return
 
     def get_prime_coords(self):
         return (self._x(self.x.get_value(), self.ct.get_value()), self._ct(self.x.get_value(), self.ct.get_value()))
     
-    def create(self):
-        self.plot_lines()
-        self.plot_point()
-        self.plot__lines()
+    def create(self, **kwargs):
+        self.plot_lines(**kwargs)
+        self.plot_point(**kwargs)
+        self.plot__lines(**kwargs)
         return 
 
     def remove(self):
@@ -169,7 +169,6 @@ class Location:
     def move_to(self, x, ct, **kwargs):
         self.diagram.scene.play(self.x.animate(**kwargs).set_value(x), self.ct.animate(**kwargs).set_value(ct))
         return
-        
 
 class PrimeLocation:
     def __init__(self, diagram, x, ct):
@@ -181,30 +180,30 @@ class PrimeLocation:
         self.everything = VGroup()
         return
 
-    def plot__lines(self):
+    def plot__lines(self, **kwargs):
         # Create the lines
         x__line = always_redraw(
-            lambda: Line(self.diagram.grid @ self.diagram.prime_to_cords(self._x.get_value(), 0), self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), color=YELLOW, stroke_width=0.5)
+            lambda: Line(self.diagram.ct__line @ self._ct.get_value(), self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), color=YELLOW, stroke_width=0.5)
         )
         ct__line = always_redraw(
-            lambda: Line(self.diagram.grid @ self.diagram.prime_to_cords(0, self._ct.get_value()), self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), color=YELLOW, stroke_width=0.5)
+            lambda: Line(self.diagram.x__line @ self._x.get_value(), self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), color=YELLOW, stroke_width=0.5)
         )
         self.everything.add(x__line, ct__line)
-        self.diagram.scene.play(Create(x__line), Create(ct__line))
+        self.diagram.scene.play(Create(x__line), Create(ct__line), **kwargs)
         self.diagram.scene.wait(0.1)
         return
 
-    def plot_point(self):
+    def plot_point(self, **kwargs):
         # Create the point
         point = always_redraw(
             lambda: Dot(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), color=RED, radius=0.04)
         )
         self.everything.add(point)
-        self.diagram.scene.play(Create(point))
+        self.diagram.scene.play(Create(point), **kwargs)
         self.diagram.scene.wait(0.1)
         return
 
-    def plot_lines(self):
+    def plot_lines(self, **kwargs):
         # Create the lines
         x_line = always_redraw(
             lambda: Line(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), self.diagram.grid @ (self.x.get_value(), 0), stroke_width=1)
@@ -213,17 +212,17 @@ class PrimeLocation:
             lambda: Line(self.diagram.grid @ (self.x.get_value(), self.ct.get_value()), self.diagram.grid @ (0, self.ct.get_value()), stroke_width=1)
         )
         self.everything.add(x_line, ct_line)
-        self.diagram.scene.play(Create(x_line), Create(ct_line))
+        self.diagram.scene.play(Create(x_line), Create(ct_line), **kwargs)
         self.diagram.scene.wait(0.1)
         return
 
     def get_coords(self):
         return (self.x.get_value(), self.ct.get_value())
     
-    def create(self):
-        self.plot__lines()
-        self.plot_point()
-        self.plot_lines()
+    def create(self, **kwargs):
+        self.plot__lines(**kwargs)
+        self.plot_point(**kwargs)
+        self.plot_lines(**kwargs)
         return
 
     def remove(self):  
--- my-project/scenes.py
+++ my-project/scenes.py
@@ -2,6 +2,18 @@ from manim import *
 from custom_classes import *
 class Scene1(Scene):
     def construct(self):
+        diagram = SpaceTimeGrid(scene=self, speed=0.8, max_number=9, count=1)
+        diagram.show()
+        location = Location(diagram=diagram, x=3, ct=3)
+        location.create()
+        location2 = PrimeLocation(diagram=diagram, x=2, ct=2)
+        location2.create()
+        print(diagram.x__line.x_range, location.diagram.x__line.x_range)
+        print(diagram.speed.get_value(), location.diagram.speed.get_value())
+        diagram.change_speed(new_speed=0.2, run_time=1)
+        print(diagram.x__line.x_range, location.diagram.x__line.x_range)
+        print(diagram.speed.get_value(), location.diagram.speed.get_value())
+        self.wait()
         return
 
 class Testing(Scene):
