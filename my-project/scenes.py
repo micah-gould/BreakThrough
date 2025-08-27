5f5bfc4abc8c0d58490c9d3ac5fc144db3685d26
micah-gould <Gouldmicah@gmail.com>
2025-08-27 22:54:29 +0200

    Updated first scene and worked on 2nd scene

--- my-project/scenes.py
+++ my-project/scenes.py
@@ -1,7 +1,9 @@
 from manim import *
 from custom_classes import *
 
-class ExplainTheDiagram(Scene): # DONE
+# TODO: Add subtitles later
+
+class ExplainTheDiagram(Scene):
     def construct(self):
         first_train_x = NumberLine(x_range=[-1, 1, 1], length=4, color=BLUE, exclude_origin_tick=True, tip_shape=StealthTip, include_tip=True).shift(UP)
 
@@ -16,7 +18,7 @@ class ExplainTheDiagram(Scene): # DONE
         self.play(me.animate.move_to(first_train_x @ (1)), rate_func=wiggle, run_time=2)
         self.wait()
         
-        first_train_t = NumberLine(x_range=[0, 2, 2], length=4, color=BLUE, include_tip=True, exclude_origin_tick=True, z_index=-1).rotate(90*DEGREES, about_point=ORIGIN).shift(first_train_x @ (-1) - ORIGIN)
+        first_train_t = NumberLine(x_range=[0, 2, 2], length=4, color=MAROON_A, include_tip=True, exclude_origin_tick=True, z_index=-1).rotate(90*DEGREES, about_point=ORIGIN).shift(first_train_x @ (-1) - ORIGIN)
 
         distance = first_train_t @ (0) - first_train_x @ (-1)
         # Animate outward from the middle
@@ -30,11 +32,11 @@ class ExplainTheDiagram(Scene): # DONE
 
         path = ParametricFunction(
             lambda t: np.array([
-                2.75*np.sin(2*PI*t/5),  # x wiggle
+                1.9*np.sin(2*PI*t/3.5),  # x wiggle
                 t,                # y upward
                 0
             ]),
-            t_range=[0, 5],  # duration of motion
+            t_range=[0, 3.5],  # duration of motion
         ).shift(first_train_x @ (0))
 
         # Animate the dot along the path
@@ -47,30 +49,53 @@ class ExplainTheDiagram(Scene): # DONE
 class TwoTrains(Scene):
     def construct(self):
         first_train_x = NumberLine(x_range=[-1, 1, 1], length=4, color=BLUE, exclude_origin_tick=True, z_index=100, tip_shape=StealthTip, include_tip=True).shift(UP)
-        first_train_t = NumberLine(x_range=[0, 2, 2], length=4, color=BLUE, include_tip=True, exclude_origin_tick=True, z_index=-1).rotate(90*DEGREES, about_point=ORIGIN).shift(first_train_x @ (-1) - ORIGIN)
+        first_train_t = NumberLine(x_range=[0, 2, 2], length=4, color=MAROON_A, include_tip=True, exclude_origin_tick=True, z_index=-1).rotate(90*DEGREES, about_point=ORIGIN).shift(first_train_x @ (-1) - ORIGIN)
         first_train_x.shift(first_train_t @ (0) - first_train_x @ (-1))
 
         self.add(first_train_x, first_train_t)
         
-        line_head = Dot(first_train_x @ (-1), color=YELLOW, z_index=1000)
-        line = always_redraw(lambda: Line(line_head.get_center(), first_train_x @ (-1), color=YELLOW, z_index=-2, stroke_width=2))
+        line_head = Dot(first_train_x @ (-1), color=MAROON_B, z_index=1000)
+        line = always_redraw(lambda: Line(line_head.get_center(), first_train_x @ (-1), color=MAROON_B, z_index=-2, stroke_width=2))
         self.play(Create(line_head), Create(line))
         self.wait()
 
-        self.play(line_head.animate.shift((first_train_x @ (0) - first_train_x @ (-1)) + (first_train_t @ (np.sqrt(3)) - first_train_t @ (0))), run_time=2)
+        self.play(line_head.animate.shift((first_train_x @ (2/np.sqrt(3) - 1) - first_train_x @ (-1)) + (first_train_t @ (2) - first_train_t @ (0))), run_time=2)
         self.wait()
 
-        second_train_t = NumberLine(x_range=[0, 2, 2], length=4, color=YELLOW, include_tip=True, exclude_origin_tick=True, z_index=-2).rotate(60*DEGREES, about_point=ORIGIN)
+        second_train_t = NumberLine(x_range=[0, 2, 2], length=8/np.sqrt(3), color=MAROON_B, include_tip=True, exclude_origin_tick=True, z_index=-2).rotate(60*DEGREES, about_point=ORIGIN)
         second_train_t.shift(first_train_x @ (-1) - second_train_t @ (0))
 
         self.play(FadeIn(second_train_t), FadeOut(line), FadeOut(line_head))
         self.wait()
 
-        second_train_x = NumberLine(x_range=[-1, 1, 1], length=4, color=YELLOW, exclude_origin_tick=True, z_index=-1, tip_shape=StealthTip, include_tip=True).rotate(-30*DEGREES, about_point=ORIGIN)
-        second_train_x.shift(first_train_x @ (-1) - second_train_x @ (-1))
+        second_train_x = NumberLine(x_range=[-1, 1, 1], length=4, color=YELLOW, exclude_origin_tick=True, z_index=101, tip_shape=StealthTip, include_tip=True)
+        second_train_x.shift(first_train_x @ (-1) - second_train_x @ (-1)).set_opacity(0.5 )
 
         self.play(Create(second_train_x))
         self.wait()
+
+        def showTrainMovmement():
+            temp = first_train_x.copy()
+            self.play(temp.animate().shift(first_train_t @ (2) - first_train_x @ (-1)))
+            self.wait()
+
+            temp2 = second_train_x.copy().set_opacity(1)
+            self.play(temp2.animate().shift(second_train_t @ (2) - second_train_x @ (-1)))
+            self.wait()
+        
+            self.play(FadeOut(temp), FadeOut(temp2))
+            self.wait()
+
+        showTrainMovmement()
+
+        temp = first_train_t.copy().set_length(8/np.sqrt(3))
+        temp2 = second_train_t.copy().set_length(4)
+        self.play(first_train_t.animate.set_length(8/np.sqrt(3)).shift(first_train_x @ (-1) - temp @ (0)).rotate(30*DEGREES, about_point=first_train_x @ (-1)), second_train_t.animate.set_length(4).shift(second_train_x @ (-1) - temp2 @ (0)).rotate(30*DEGREES, about_point=second_train_x @ (-1)))
+        self.wait()
+
+        showTrainMovmement()
+
+        # TODO: add more?
 
         return 
 
